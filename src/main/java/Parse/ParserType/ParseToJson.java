2acf0989edb312be2ca4e25ffc2838c5ca04a3fa
Liran <Liranpro54@gmail.com>
2022-01-17 14:52:42 +0200

    adding the parsing to json code, not finished

--- src/main/java/Parse/ParserType/ParseToJson.java
+++ src/main/java/Parse/ParserType/ParseToJson.java
@@ -1,15 +1,34 @@
 package Parse.ParserType;
 
 import Parse.AbstractClass.Parser;
+import org.json.simple.JSONArray;
 import java.io.IOException;
 
 public class ParseToJson extends Parser {
+    private JSONArray tests;
+    private final int limit = 50000;
 
-    public ParseToJson(String path, String targetPath) throws IOException {
+    protected int getLimit() {
+        return limit;
+    }
+    public JSONArray getTests() {
+        return tests;
+    }
+
+    public ParseToJson(String targetPath, String path) throws IOException {
+        super(path);
+        setTargetPath(targetPath);
     }
 
     @Override
     protected void parseData() {
 
+        while(!getLines().isEmpty()){
+            for (int i = 0; i < getLimit(); i++) {
+                getLines().pop();
+            }
+        }
+
+        getLines();
     }
 }
