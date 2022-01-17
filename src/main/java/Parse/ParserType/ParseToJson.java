6ea6fc673dd3ce05eceae27aec936e1954d00699
Liran <Liranpro54@gmail.com>
2022-01-17 14:32:44 +0200

    changed the flow

--- src/main/java/Parse/ParserType/ParseToJson.java
+++ src/main/java/Parse/ParserType/ParseToJson.java
@@ -1,2 +1,15 @@
-package Parse.ParserType;public class ParseToJson {
+package Parse.ParserType;
+
+import Parse.AbstractClass.Parser;
+import java.io.IOException;
+
+public class ParseToJson extends Parser {
+
+    public ParseToJson(String path, String targetPath) throws IOException {
+    }
+
+    @Override
+    protected void parseData() {
+
+    }
 }
