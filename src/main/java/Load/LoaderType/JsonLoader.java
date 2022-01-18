1d6c41ec4e1e113686ad7ac8cc81300ba8fdec1e
Liran <Liranpro54@gmail.com>
2022-01-18 15:22:42 +0200

    changed the class because of the interface change

--- src/main/java/Load/LoaderType/JsonLoader.java
+++ src/main/java/Load/LoaderType/JsonLoader.java
@@ -1,46 +1,33 @@
 package Load.LoaderType;
 
-import Load.Interface.Loader;
+import Load.AbstractClass.Loader;
 import Parse.ParserType.ParseToJson;
 import org.json.simple.JSONArray;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class JsonLoader implements Loader {
+public class JsonLoader extends Loader {
     private int limit;
-    private String path;
-    private String target;
 
     //cTor
     public JsonLoader(String path, String targetDir) throws IOException {
         setPath(path);
         setLimit(50000);
         setTarget(targetDir);
-        LoadToDir(new ParseToJson(getPath()).getTests());
+        LoadToDir();
     }
 
     //getters & setters
     protected int getLimit() {
         return limit;
     }
-    protected String getPath() {
-        return path;
-    }
-    protected String getTarget() {
-        return target;
-    }
-    public void setPath(String path) {
-        this.path = path;
-    }
     public void setLimit(int limit) {
         this.limit = limit;
     }
-    protected void setTarget(String target) {
-        this.target = target;
-    }
 
-    @Override
-    public void LoadToDir(JSONArray tests) throws IOException {
+    public void LoadToDir() throws IOException {
+        JSONArray tests = new ParseToJson(getPath()).getTests();
+
         int currLength = 0;
         int newLength = 0;
         int fileIndex = 1;
