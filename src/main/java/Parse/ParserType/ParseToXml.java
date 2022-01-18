967f2e4d69fdfa5e147ec6af44b72b0cc1b394f7
Liran <Liranpro54@gmail.com>
2022-01-18 10:00:53 +0200

    added cTor

--- src/main/java/Parse/ParserType/ParseToXml.java
+++ src/main/java/Parse/ParserType/ParseToXml.java
@@ -2,8 +2,15 @@ package Parse.ParserType;
 
 import Extract.Types.CsvExtract;
 import Parse.Interface.Parser;
+import java.io.IOException;
 
 public class ParseToXml implements Parser {
+    
+    //cTor
+    public ParseToXml(String path) throws IOException {
+        parseData(new CsvExtract(path));
+    }
+
     @Override
     public void parseData(CsvExtract newStack) {
 
