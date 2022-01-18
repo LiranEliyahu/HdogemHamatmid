967684921140ab1dc5c95fc956a8a1ce3e53ca9e
Liran <Liranpro54@gmail.com>
2022-01-18 09:21:49 +0200

    changed the fields in the enum of stage b

--- src/main/java/Parse/Enum/FieldsOfStageB.java
+++ src/main/java/Parse/Enum/FieldsOfStageB.java
@@ -1,2 +1,25 @@
-package Parse.Enum;public enum FieldsOfStageB {
+package Parse.Enum;
+
+public enum FieldsOfStageB {
+    IDNUM("IDNum"),
+    IDTYPE("IDType"),
+    FIRSTNAME("FirstName"),
+    LASTNAME("LastName"),
+    RESULTDATE("ResultDate"),
+    BIRTHDATE("BirthDate"),
+    LABCODE("LabCode"),
+    STICKERNUMBER("StickerNumber"),
+    RESULTTESTCORONA("ResultTestCorona"),
+    VARIANT("Variant"),
+    TESTTYPE("TestType");
+
+    private final String field;
+
+    FieldsOfStageB(String value) {
+        this.field = value;
+    }
+
+    public String getField() {
+        return field;
+    }
 }
