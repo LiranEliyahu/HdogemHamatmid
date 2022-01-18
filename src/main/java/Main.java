07bf219c9fb02bcdfc46e79882ed8f11a6e9436c
Liran <Liranpro54@gmail.com>
2022-01-18 09:21:09 +0200

    changed one line in the main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,8 @@ import Load.JsonLoader.JsonLoader;
 public class Main {
     public static void main(String[] args) {
         try{
-            new JsonLoader("C:\\Users\\liran\\Downloads\\MadaReports - MadaReports.csv", "C:\\Users\\liran\\Downloads\\test\\");
+            new JsonLoader("C:\\Users\\liran\\Downloads\\MadaReports - MadaReports.csv",
+                    "C:\\Users\\liran\\Downloads\\test\\");
         }
         catch (Exception e){
             System.out.println(e);
