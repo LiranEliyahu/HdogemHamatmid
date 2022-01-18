8d9a599b6076a97a9ee53d9c53bd055818082855
Liran <Liranpro54@gmail.com>
2022-01-18 09:43:31 +0200

    now implemanting Parser interface

--- src/main/java/Parse/ParserType/ParseToXml.java
+++ src/main/java/Parse/ParserType/ParseToXml.java
@@ -1,2 +1,11 @@
-package Parse.ParserType;public class ParseToXml {
+package Parse.ParserType;
+
+import Extract.Types.CsvExtract;
+import Parse.Interface.Parser;
+
+public class ParseToXml implements Parser {
+    @Override
+    public void parseData(CsvExtract newStack) {
+
+    }
 }
