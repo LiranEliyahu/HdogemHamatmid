2bbd0dfec7cebd07dde067e024609a3548a64215
Liran <Liranpro54@gmail.com>
2022-01-18 08:14:31 +0200

    added some things to the class

--- src/main/java/Extract/Types/CsvExtract.java
+++ src/main/java/Extract/Types/CsvExtract.java
@@ -6,22 +6,23 @@ import java.util.Stack;
 import Extract.AbstractClass.ExtractFile;
 
 public class CsvExtract extends ExtractFile {
-    private Stack<String[]> lines;
+    private final Stack<String[]> lines = new Stack<>();
 
+    //cTor
     public CsvExtract(String path) throws IOException {
         setPath(path);
         extractInfo();
     }
 
-    protected Stack<String[]> getLines() {
-        return lines;
+    //getter
+    public Stack<String[]> getLines() {
+        return this.lines;
     }
 
     @Override
     protected void extractInfo() throws IOException {
         BufferedReader file = getFile();
         String row;
-
         while ((row = file.readLine()) != null) {
             String[] data = row.split(",");
             getLines().push(data);
