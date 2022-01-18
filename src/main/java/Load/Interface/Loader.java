2321820f629b4fb076460b79ed424fbf1a81a6fe
Liran <Liranpro54@gmail.com>
2022-01-18 08:14:56 +0200

    added the interface to the loader

--- src/main/java/Load/Interface/Loader.java
+++ src/main/java/Load/Interface/Loader.java
@@ -1,2 +1,8 @@
-package Load.Interface;public interface Loader {
+package Load.Interface;
+
+import org.json.simple.JSONArray;
+import java.io.IOException;
+
+public interface Loader {
+    void LoadToDir(JSONArray tests) throws IOException;
 }
