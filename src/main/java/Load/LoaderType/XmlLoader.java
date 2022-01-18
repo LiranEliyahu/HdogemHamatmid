025ce7295481c2579f3801fef2c2ec88dbed8249
Liran <Liranpro54@gmail.com>
2022-01-18 15:23:24 +0200

    changed the flow of the package so i've changed the class a little bit

--- src/main/java/Load/LoaderType/XmlLoader.java
+++ src/main/java/Load/LoaderType/XmlLoader.java
@@ -1,2 +1,18 @@
-package Load.LoaderType;public class XmlLoader {
+package Load.LoaderType;
+
+import Load.AbstractClass.Loader;
+import java.io.IOException;
+
+public class XmlLoader extends Loader {
+    public XmlLoader(String path, String target) throws IOException {
+        setPath(path);
+        setTarget(target);
+        LoadToDir();
+    }
+
+
+    @Override
+    public void LoadToDir() throws IOException {
+
+    }
 }
