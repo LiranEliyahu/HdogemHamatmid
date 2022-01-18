4e11f097ca58b18bc46cbbf67a654d29e9a64bec
Liran <Liranpro54@gmail.com>
2022-01-18 08:12:41 +0200

    added the main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+import Load.JsonLoader.JsonLoader;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            new JsonLoader("C:\\Users\\liran\\Downloads\\MadaReports - MadaReports.csv", "C:\\Users\\liran\\Downloads\\test\\");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
